b70914fdff7825d25e947b53cac7ab4234c57a16
Jonathanks <barry.jonathanks@gmail.com>
2017-10-05 12:13:54 +0100

    Implemented MapHandler and YAMLHandler. This satisfies the requirements of Exercise #2 (https://github.com/gophercises/urlshort/blob/master/README.md)

--- handler.go
+++ handler.go
@@ -2,6 +2,8 @@ package urlshort
 
 import (
 	"net/http"
+
+	yaml "gopkg.in/yaml.v2"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -11,8 +13,14 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	// TODO: Implement this...
-	return nil
+	return func(w http.ResponseWriter, r *http.Request) {
+		path, ok := pathsToUrls[r.URL.Path]
+		if ok {
+			http.Redirect(w, r, path, http.StatusFound)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -31,7 +39,26 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 //
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
-func YAMLHandler(yaml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	dst := []map[string]string{}
+	if err := yaml.Unmarshal(yml, &dst); err != nil {
+		return nil, err
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		path, ok := pathSeen(r.URL.Path, &dst)
+		if ok {
+			http.Redirect(w, r, path, http.StatusFound)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}, nil
+}
+
+func pathSeen(path string, pathMapSlc *[]map[string]string) (string, bool) {
+	for _, entry := range *pathMapSlc {
+		if entry["path"] == path {
+			return entry["url"], true
+		}
+	}
+	return "", false
 }
