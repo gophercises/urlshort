81b557230f3a905a14c0e2ec6e1654dd5336957f
Jonathan Barisere <barry.jonathanks@gmail.com>
2017-10-06 08:47:54 +0100

    Implemented MapHandler and YAMLHandler. (#1)
    * Implemented MapHandler and YAMLHandler.
    This satisfies the requirements of Exercise #2 (https://github.com/gophercises/urlshort/blob/master/README.md)
    
    * Moved implementation into nested directory students/latentgenius
    
    * Reused MapHandler from YAMLHandler

--- students/latentgenius/handler.go
+++ students/latentgenius/handler.go
@@ -1,7 +1,9 @@
-package urlshort
+package latentgenius
 
 import (
 	"net/http"
+
+	yamlV2 "gopkg.in/yaml.v2"
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -11,8 +13,14 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	return func(w http.ResponseWriter, r *http.Request) {
+		path, ok := pathsToUrls[r.URL.Path]
+		if ok {
+			http.Redirect(w, r, path, http.StatusFound)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -31,7 +39,25 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 //
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
-func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+func YAMLHandler(yaml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	parsedYaml, err := parseYAML(yaml)
+	if err != nil {
+		return nil, err
+	}
+	pathMap := buildMap(parsedYaml)
+	return MapHandler(pathMap, fallback), nil
+}
+
+func parseYAML(yaml []byte) (dst []map[string]string, err error) {
+	err = yamlV2.Unmarshal(yaml, &dst)
+	return dst, err
+}
+
+func buildMap(parsedYaml []map[string]string) map[string]string {
+	mergedMap := make(map[string]string)
+	for _, entry := range parsedYaml {
+		key := entry["path"]
+		mergedMap[key] = entry["url"]
+	}
+	return mergedMap
 }
