bcb679d6a4f2c48af4016f20a966832883529460
Jonathanks <barry.jonathanks@gmail.com>
2017-10-06 13:31:32 +0100

    Load URL map from YAML file instead of a string.

--- students/latentgenius/main/main.go
+++ students/latentgenius/main/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/gophercises/urlshort/students/latentgenius"
 )
 
+var (
+	yamlPath string
+)
+
+func init() {
+	flag.StringVar(&yamlPath, "yaml", "", "path to yaml file")
+	flag.Parse()
+}
+
 func main() {
 	mux := defaultMux()
-
 	// Build the MapHandler using the mux as the fallback
 	pathsToUrls := map[string]string{
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
@@ -17,20 +28,20 @@ func main() {
 	}
 	mapHandler := latentgenius.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/final
-`
-	yamlHandler, err := latentgenius.YAMLHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
+	if yamlPath != "" {
+		yamlData, err := ioutil.ReadFile(yamlPath)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		// Build the YAMLHandler using the mapHandler as the
+		// fallback
+		yamlHandler, err := latentgenius.YAMLHandler(yamlData, mapHandler)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Starting the server on :8080")
+		http.ListenAndServe(":8080", yamlHandler)
 	}
-	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
 }
 
 func defaultMux() *http.ServeMux {
