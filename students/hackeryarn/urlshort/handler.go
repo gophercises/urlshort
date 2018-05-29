87c3b44afe22e11b56baac4915f238fe4ff23295
achernyak <artemchernyak@gmail.com>
2018-05-29 07:06:55 -0500

    finished maphandler

--- students/hackeryarn/urlshort/handler.go
+++ students/hackeryarn/urlshort/handler.go
@@ -12,6 +12,13 @@ import (
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		}
+
 		fallback.ServeHTTP(w, r)
 	}
 }
--- students/hackeryarn/urlshort/handler_test.go
+++ students/hackeryarn/urlshort/handler_test.go
@@ -2,6 +2,7 @@ package urlshort
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -14,25 +15,74 @@ func fallback(w http.ResponseWriter, r *http.Request) {
 }
 
 func TestMapHandler(t *testing.T) {
-	mapHandler := createMapHandler()
-
 	t.Run("it uses the fallback for unknown routes", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/unknown", nil)
-		response := httptest.NewRecorder()
+		result := runMapHandler("", "")
 
-		mapHandler(response, request)
+		assertBody(t, result, fallbackResponse)
+	})
 
-		got := response.Body.String()
+	t.Run("it redirects for found url", func(t *testing.T) {
+		path := "/test"
+		dest := "https://test.com"
 
-		if got != fallbackResponse {
-			t.Errorf("Expected fallback response to be %s, got %v",
-				fallbackResponse, got)
-		}
+		result := runMapHandler(path, dest)
+
+		assertStatus(t, result, http.StatusFound)
+		assertURL(t, result, dest)
 	})
 }
 
-func createMapHandler() http.HandlerFunc {
-	pathToUrls := map[string]string{}
+func createMapHandler(pathToUrls map[string]string) http.HandlerFunc {
 	fallbackHandler := http.HandlerFunc(fallback)
 	return MapHandler(pathToUrls, fallbackHandler)
+}
+
+func runMapHandler(path, dest string) *http.Response {
+	pathToUrls := map[string]string{path: dest}
+	mapHandler := createMapHandler(pathToUrls)
+
+	if path == "" {
+		path = "/unknown"
+	}
+	request, _ := http.NewRequest(http.MethodGet, path, nil)
+	response := httptest.NewRecorder()
+
+	mapHandler(response, request)
+	return response.Result()
+}
+
+func assertURL(t *testing.T, resp *http.Response, want string) {
+	t.Helper()
+	url, err := resp.Location()
+
+	if err != nil {
+		t.Fatal("Could not read location", err)
+	}
+
+	if url.String() != want {
+		t.Errorf("Expected url to be %s, got %s", url, want)
+	}
+}
+
+func assertStatus(t *testing.T, resp *http.Response, want int) {
+	t.Helper()
+	if resp.StatusCode != want {
+		t.Errorf("Expected status to be %d, got %d",
+			want, resp.StatusCode)
+	}
+}
+
+func assertBody(t *testing.T, resp *http.Response, want string) {
+	t.Helper()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		t.Fatal("Could not ready response body", err)
+	}
+
+	got := string(body)
+	if want != got {
+		t.Errorf("Expected response body to be %s, got %s",
+			want, got)
+	}
 }
