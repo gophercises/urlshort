e897a198de808550dcf070ad2cd42cc1290a314e
Servio Zambrano <servio.za@gmail.com>
2020-04-26 15:38:38 -0400

    fenriz solution

--- students/fenriz/main/main.go
+++ students/fenriz/main/main.go
@@ -1,36 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 
-	"github.com/gophercises/urlshort"
+	"github.com/fenriz07/urlshort"
 )
 
 func main() {
 	mux := defaultMux()
+
+	yamlfile := flag.String("yamlfile", "redirect.yaml", "name to yaml file examplñe redirect.yaml")
+	flag.Parse()
 
 	// Build the MapHandler using the mux as the fallback
 	pathsToUrls := map[string]string{
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
+
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
-	}
+	mapHandler = urlshort.YAMLHandler(*yamlfile, mapHandler)
+
+	mapHandler = urlshort.JSONHandler("redirect.json", mapHandler)
+
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", mapHandler)
 }
 
 func defaultMux() *http.ServeMux {
