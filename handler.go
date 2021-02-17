6a37af003dd12fb7838244e97a87e13f841019b6
Elias Howell <ejh@snarf.top>
2021-02-17 14:53:44 -0600

    add urlshort

--- handler.go
+++ handler.go
@@ -1,37 +1,35 @@
 package urlshort
 
 import (
+	"gopkg.in/yaml.v2"
 	"net/http"
 )
 
-// MapHandler will return an http.HandlerFunc (which also
-// implements http.Handler) that will attempt to map any
-// paths (keys in the map) to their corresponding URL (values
-// that each key in the map points to, in string format).
-// If the path is not provided in the map, then the fallback
-// http.Handler will be called instead.
-func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+func MapHandler(paths map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if destination, ok := paths[path]; ok {
+			http.Redirect(w, r, destination, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(w, r)
+	}
 }
 
-// YAMLHandler will parse the provided YAML and then return
-// an http.HandlerFunc (which also implements http.Handler)
-// that will attempt to map any paths to their corresponding
-// URL. If the path is not provided in the YAML, then the
-// fallback http.Handler will be called instead.
-//
-// YAML is expected to be in the format:
-//
-//     - path: /some-path
-//       url: https://www.some-url.com/demo
-//
-// The only errors that can be returned all related to having
-// invalid YAML data.
-//
-// See MapHandler to create a similar http.HandlerFunc via
-// a mapping of paths to urls.
-func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+func YAMLHandler(yamlinbytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	var pathandurl []urlpath
+	err := yaml.Unmarshal(yamlinbytes, &pathandurl)
+	if err != nil {
+		return nil, err
+	}
+	paths := make(map[string]string)
+	for _, pu := range pathandurl {
+		paths[pu.Path] = pu.URL
+	}
+	return MapHandler(paths, fallback), nil
+}
+
+type urlpath struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
 }
