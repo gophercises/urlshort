a4c8545136bd59efdffe49404ff61ef0ecdc9c45
Szymon Mikitiuk <szymon.mikitiuk@infermedica.com>
2023-11-11 17:54:27 +0100

    Implement MapHandler

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -1,6 +1,7 @@
 package urlshort
 
 import (
+	"log/slog"
 	"net/http"
 )
 
@@ -11,8 +12,17 @@ import (
 // If the path is not provided in the map, then the fallback
 // http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
-	//	TODO: Implement this...
-	return nil
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("Request url: " + r.URL.String())
+		path := r.URL.Path
+		if url, exists := pathsToUrls[path]; exists {
+			slog.Info("Redirecting to " + url)
+			http.Redirect(w, r, url, http.StatusMovedPermanently)
+			return
+		}
+		slog.Warn("No url in map")
+		fallback.ServeHTTP(w, r)
+	}
 }
 
 // YAMLHandler will parse the provided YAML and then return
@@ -23,8 +33,8 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 //
 // YAML is expected to be in the format:
 //
-//     - path: /some-path
-//       url: https://www.some-url.com/demo
+//   - path: /some-path
+//     url: https://www.some-url.com/demo
 //
 // The only errors that can be returned all related to having
 // invalid YAML data.
